34fa6361ea3b3aef362e9dac55e0b47c56b75d00
Jefferson Abreu <nabucodonosor310@gmail.com>
2020-04-15 08:33:11 -0300

    criado primeiro componente

---
+++ src/app.controller.js
@@ -0,0 +1,8 @@
+function AppController($scope) {
+  let vm = this;
+  vm.titulo = "Bem vindo";
+}
+
+AppController.$inject = ["$scope"];
+
+export default AppController;
---
+++ src/app.js
@@ -0,0 +1,4 @@
+import angular from "angular";
+import appController from "./app.controller";
+
+angular.module("app").controller("AppController", appController).name;
